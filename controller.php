e49b2d1dc2355028e66124eed64183f8a543df30
Eliot Horowitz <eliot@10gen.com>
2008-08-21 09:32:28 -0400

    basics working

---
+++ controller.php
@@ -0,0 +1,19 @@
+<?
+
+echo $_10gen["request"]->getURI() . "<br>";
+
+$val = $db["things"]->findOne( array( "name" => $request["name"] ) );
+
+if ( $val ){
+  $val["num"] = $val["num"] + 1;
+}
+else {
+  $val = array( "name" => $request["name"] , "num" => 1 );
+}
+    
+$db["things"]->save( $val );
+
+echo "name: <b>" . $val["name"] . "</b><br>";
+echo "count: <b>" . $val["num"] . "</b><br>";
+
+?>
